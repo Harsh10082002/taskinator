14271bfa3fe1ff55db3367839fe8a2657fce1ba1
and <choukseyharsh696@gmail.com>
2023-02-10 10:27:40 -0800

    tp

--- ind.js
+++ ind.js
@@ -1,7 +1,9 @@
+
 var task_btn = document.querySelector("#add-task-btn")
 task_btn.addEventListener("click", function(event){event.preventDefault()})
 
 function fun(){
+    var task_counter = 0;
     var task_field = document.querySelector("input[name='task-name']")
 
 
@@ -14,15 +16,28 @@ function fun(){
     var delete_btn = document.createElement('button')
     delete_btn.className = "delete_btn"
     delete_btn.textContent = "Delete"
+    
 
     list_item.textContent = task_field.value;
     list_item.className = "li"
+    list_item.setAttribute("data-id", task_counter)
+    console.log(list_item)
+    
+
     if(task_field.selectionStart >= 1){
         list_item.appendChild(edit_btn)
         list_item.appendChild(delete_btn)
         unodered_list.appendChild(list_item)
     }
-    var dlt_btn = document.querySelector(".delete_btn")
-    console.dir(dlt_btn)
-}
+    task_counter++;
 
+    dltbtn = document.querySelector(".delete_btn")
+    dltbtn.addEventListener("click", function(event) {  
+        if (event.target.task_counter === 'delete-btn') {
+
+            console.log(task_counter)
+        }
+    }
+    )
+    
+}
